9eeb28ea9e7c298deff9b16835a9b17296d4fff5
Kane Cohen <kanecohen@gmail.com>
2016-09-12 06:19:51 -0700

    Page refresh fix.

--- test/display_environment.js
+++ test/display_environment.js
@@ -5,8 +5,11 @@
 
   $('.simple-pager').html(collection.links().el);
 
-  var collection2 = new Backbone.PageableCollection([], {showPerPageSelector: true});
+  var collection2 = new Backbone.PageableCollection([], {url: 'data.json', linkUrl: 'view.html', showPerPageSelector: true});
+  collection2.on('reset', function() {
+    $('.pager-with-display').html(collection2.links().el);
+  });
   collection2.setState({total: 50});
-  $('.pager-with-display').html(collection2.links().el);
+  collection2.reset();
 
 })();
